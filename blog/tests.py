788bbff817830dde62659e910efb68f7faac8dcf
MaksymLaskavchuk <maksim.laskavcuk@gmail.com>
2023-07-22 21:23:17 +0300

    test

--- blog/tests.py
+++ blog/tests.py
@@ -11,7 +11,22 @@ def test_posts(client, faker):
     assert response.status_code == 200
 
     assert len(response.context['posts']) == Post.objects.count()
+    date = faker.date()
+    year = date[:4]
+    month = date[5:-3]
+    day = date[8:]
+    slug = faker.slug()
 
+    response = client.get(reverse('post', args=(year, day, month, slug,)))
+    assert response.status_code == 404
+    post = Post.objects.create(
+            title=faker.word(),
+            slug=faker.slug(),
+            author=User.objects.create_user(username=faker.word(),
+                                            password=faker.password()),
+            body=faker.sentences(),
+            status=Status.PUBLISHED
+        )
     for _ in range(2):
         Post.objects.create(
             title=faker.word(),
@@ -21,6 +36,12 @@ def test_posts(client, faker):
             body=faker.sentences(),
             status=Status.PUBLISHED
         )
+    response = client.get(reverse('post', args=(post.created.year,
+                                                post.created.month,
+                                                post.created.day,
+                                                post.slug
+                                                )))
+    assert response.status_code == 200
 
     url = reverse('posts')
     response = client.get(url)
